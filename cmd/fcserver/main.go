775ddcda9abacb8f30b5395432638f0f001abf1c
Otto Roming <otro@lysator.liu.se>
2026-02-08 19:12:02 +0100

    fcserver

---
+++ cmd/fcserver/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	// address = "localhost:4040"
+)
+// err := fcmul.Unmarshal(`
+	// 	{
+	// 		"Uwu" -> "rawr"
+	// 	}
+	// `, &test)
+	// if err != nil {
+	// 	fmt.Printf("err: %v\n", err)
+	// }
+
+
+// func handleConnection(conn net.Conn) {
+// 	defer conn.Close()
+
+// 	msg, err := fcprotocol.Parse(conn)
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	fmt.Printf("msg: %v\n", msg)
+// }
+
+
+func main() {
+	value := ""
+	// fcmul.Unmarshal(`"test"`, &value)
+	fmt.Printf("value: %v\n", value)
+
+	fmt.Printf("math.MaxInt64: %s\n", fmt.Sprint(math.MaxInt64))
+
+	// listener, err := net.Listen("tcp", address)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// defer listener.Close()
+
+	// fmt.Printf("Listening on %s\n", address)
+
+	// for {
+	// 	conn, err := listener.Accept()
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+
+	// 	go handleConnection(conn)
+	// }
+}
